f22f7e205754d5db0555eccc38dc733e39907e9d
Vasileios Pasialiokis <whiterocket@outlook.com>
2024-06-07 04:33:52 +0300

    update

--- _src/cv/fonts.php
+++ _src/cv/fonts.php
@@ -1,4 +1,4 @@
 @font-face {
 	font-family: 'Playfair Display';
-	src: url(<?= data_url('data:font/truetype;charset=utf-8', './assets/playfair-display/PlayfairDisplay-VariableFont_wght.ttf') ?>);
+	src: url(<?= data_url('data:font/truetype;charset=utf-8', './_src/cv/assets/playfair-display/PlayfairDisplay-VariableFont_wght.ttf') ?>);
 }

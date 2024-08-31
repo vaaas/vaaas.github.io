fc80b14de1e7974e975d1ed0dbf95a9a6a0001d9
Vasileios Pasialiokis <whiterocket@outlook.com>
2024-08-31 13:22:52 +0300

    change email

--- _src/cv/fragments/header.php
+++ _src/cv/fragments/header.php
@@ -3,7 +3,7 @@
 
 	<section class='children:block mobile:block'>
 		<div>full-stack developer</div>
-		<a class='cursor-pointer hover:underline leading-2hp pt-2p' href='mailto:pasivasi@outlook.com'>pasivasi@outlook.com</a>
+		<a class='cursor-pointer hover:underline leading-2hp pt-2p' href='mailto:pasivasi@outlook.com'>vas@tsuku.ro</a>
 		<a class='cursor-pointer hover:underline leading-2hp' href='tel:+306955018104'>+30 695 501 8104</a>
 		<a class='cursor-pointer hover:underline leading-2hp' href='https://github.com/vaaas'>github.com/vaaas</a>
 	</section>
